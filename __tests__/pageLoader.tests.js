6b8056823647ed2b3bbb7a0375ba768f59dc69b1
fairwind2k <fairwind@md2k.net>
2024-02-19 23:26:36 +0100

    fix code

--- __tests__/pageLoader.tests.js
+++ __tests__/pageLoader.tests.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-tabs */
 import { test, expect, beforeEach } from '@jest/globals';
-import { path } from 'path';
-// import { fileURLToPath } from 'url';
+import { dirname, join, path } from 'path';
+import { fileURLToPath } from 'url';
 import fs from 'fs/promises';
 import os from 'os';
 import nock from 'nock';
@@ -11,11 +11,11 @@ nock.disableNetConnect();
 
 let pathToTmpDir;
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const getFixturePath = (filename) => join(__dirname, '../__fixtures__', filename);
-// const testFilePath = getFixturePath('file1.html');
+const getFixturePath = (filename) => join(__dirname, '../__fixtures__', filename);
+const testFilePath = getFixturePath('file1.html');
 
 beforeEach(async () => {
   pathToTmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
@@ -37,6 +37,6 @@ test('should be read file on the given path', async () => {
     .replyWithFile(200, `${pathToTmpDir}ru-hexlet-io-courses.html`);
 
   const expected = scope.replyWithFile();
-  // const contents = await fs.readFile(testFilePath, { encoding: 'utf8' });
-  expect(expected).toBe(true);
+  const contents = await fs.readFile(testFilePath, { encoding: 'utf8' });
+  expect(expected).toBe(contents);
 });
