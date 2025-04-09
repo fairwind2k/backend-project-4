b33cb1263b1d6d0aa80ed49fa056a149bc9e93d5
Nataliia Flerko <fairwind@macbookpro.md2k.local>
2025-04-09 22:10:41 +0200

    fix code

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -11,7 +11,7 @@ program
   .option('-o, --output [dir]', 'output dir', process.cwd())
   // .option('-o, --output [dir]', 'output dir', '/home/user/current-dir')
   .arguments('<url>')
-  .action(() => {
+  .action((url, option) => {
     const options = program.opts();
     const outputPath = pageloader(url, options.output);
     console.log(outputPath);
@@ -20,6 +20,6 @@ program
     // console.log(pageloader('https://ru.hexlet.io'));
   });
 
-program.parse();
+program.parse(process.argv);
 
 
