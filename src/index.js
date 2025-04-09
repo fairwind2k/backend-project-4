df9fe1ef6da9c7f010dc855c5fb745097eda9b67
fairwind2k <fairwind@md2k.net>
2025-04-09 23:19:47 +0200

    minor fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import downloadPage from './loader.js';
 
-const pageloader = (url, dir) => 
-  downloadPage(url, dir)
-    .then(dirName => dirName);
+const pageloader = (url, dir) =>
+  downloadPage(url, dir).then((dirName) => dirName);
 
 export default pageloader;
