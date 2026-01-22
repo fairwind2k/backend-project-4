5f4dd71f3e8091db29053ae83cdb6257656bc744
fairwind2k <fairwind@md2k.net>
2026-01-22 22:20:36 +0100

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+
+module.exports = {
+  testEnvironment: 'node',
+
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/build/'
+  ],
+
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/'
+  ],
+
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+
+  testTimeout: 5000,
+
+  verbose: true,
+};
