462c32b6a1c4ccf5c68ecf27ec19d31d8b41d11d
fairwind2k <fairwind@md2k.net>
2026-03-05 00:32:38 +0100

    refactor: extract parsers into utils block

---
+++ src/utils/parsers.js
@@ -0,0 +1,69 @@
+import path, { dirname, join } from 'path'
+import * as cheerio from 'cheerio'
+import { getHtmlFileName, getAssetPath } from "./file-name"
+import { isLocalResource } from "./validators"
+
+const prepareFileData = (response, url, dir) => {
+  const fileName = getHtmlFileName(url)
+  const filePath = path.join(dir, fileName)
+  return {
+    filePath,
+    htmlContent: response.data,
+    data: response.data,
+  }
+}
+
+const findLocalResources = (url, $, dir) => {
+  const localResources = [] 
+
+  const resourceSelectors = [
+    { selector: 'img', attr: 'src' },
+    { selector: 'link', attr: 'href' },
+    { selector: 'script', attr: 'src' },
+  ]
+
+  resourceSelectors.forEach(({ selector, attr }) => {
+    $(selector).each((i, element) => {
+      const resourceSrc = $(element).attr(attr)
+      if (resourceSrc && isLocalResource(url, resourceSrc)) {
+        const fullResourceUrl = new URL(resourceSrc, url).href
+        const resourceName = getAssetPath(url, resourceSrc)
+        const resourcePath = path.join(dir, resourceName)
+
+        localResources.push({
+          element,
+          tagName: selector,
+          attrName: attr,
+          fullUrl: fullResourceUrl,
+          localPath: resourcePath,
+          relativePath: resourceName,
+        })
+      }
+    })
+  })
+
+  return localResources 
+}
+
+const updateHtmlWithLocalPaths = ($, localResources, downloadResults) => {
+  let successCount = 0
+
+  localResources.forEach((resource, index) => {
+    const downloadResult = downloadResults[index]
+    if (downloadResult && downloadResult.status === 'success') {
+      $(resource.element).attr(resource.attrName, resource.relativePath)
+      successCount++
+    }
+  })
+
+  return {
+    updatedHtml: $.html(),
+    successCount,
+  }
+}
+
+export {
+    prepareFileData,
+    findLocalResources,
+    updateHtmlWithLocalPaths
+}
