a50cd2ec555d6766c6f26153958fd22fe1b93382
fairwind2k <fairwind@md2k.net>
2026-03-05 00:29:14 +0100

    refactor: extract error handlers into separate module

---
+++ src/errors/handlers.js
@@ -0,0 +1,37 @@
+const handleHttpError = (error, url) => {
+  if (error.code === 'HTTP_ERROR') {
+    throw error
+  }
+  
+  if (error.response) {
+    const httpError = new Error(`HTTP ${error.response.status}: Failed to load ${url}`)
+    httpError.code = 'HTTP_ERROR'
+    throw httpError
+  }
+  
+  if (error.request) {
+    const networkError = new Error(`Network error: No response from ${url}`)
+    networkError.code = 'NETWORK_ERROR'
+    throw networkError
+  }
+  
+  throw error
+}
+
+const handleFileWriteError = (error, filePath, dir) => {
+  if (error.code === 'EACCES') {
+    const permError = new Error(`Permission denied: Cannot write file to ${filePath}`)
+    permError.code = 'EACCES'
+    throw permError
+  }
+  
+  if (error.code === 'ENOENT') {
+    const dirError = new Error(`Directory does not exist: ${dir}`)
+    dirError.code = 'ENOENT'
+    throw dirError
+  }
+  
+  throw error
+}
+
+export { handleFileWriteError, handleHttpError }
