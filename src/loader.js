82f8ff72c197d5811340ff47096d395eaa035a19
fairwind2k <fairwind@md2k.net>
2026-03-05 00:35:12 +0100

    refactor: split pageloader into modules

--- src/loader.js
+++ src/loader.js
@@ -3,115 +3,19 @@ import axiosDebug from 'axios-debug-log'
 import path from 'path'
 import fs from 'fs/promises'
 import * as cheerio from 'cheerio'
-import { getHtmlFileName, getDirName, getAssetPath } from './utils/file-name.js'
 import { log } from './logger.js'
 import Listr from 'listr'
 // import { log, logParser, logFs, logError } from './logger.js'
+import { isValidUrl } from './utils/validators.js'
+import { getHtmlFileName, getDirName } from './utils/file-name.js'
+import { checkDirectory } from './modules/directory-manger.js'
+import { findLocalResources, updateHtmlWithLocalPaths } from './utils/parsers.js'
+import { createDownloadTasks } from './utils/task-builder.js'
+
+import getHtmlPage from './modules/html-loader.js'
+
 
 axiosDebug(axios)
-
-function getHtmlPage(url, dir = process.cwd()) {
-  return axios.get(url)
-    .then((response) => {
-      if (response.status !== 200) {
-        const error = new Error(`HTTP ${response.status}: Failed to load page`)
-        error.code = 'HTTP_ERROR'
-        throw error
-      }
-      const fileName = getHtmlFileName(url)
-      const filePath = path.join(dir, fileName)
-
-      return fs.writeFile(filePath, response.data)
-        .then(() => ({
-          filePath,
-          htmlContent: response.data,
-        }))
-        .catch((error) => {
-          if (error.code === 'EACCES') {
-            console.error(`Error: Permission denied. Cannot write file to ${filePath}`)
-          }
-          else if (error.code === 'ENOENT') {
-            console.error(`Error: Directory does not exist: ${dir}`)
-          }
-          else {
-            console.error(`Error: Failed to save HTML file: ${error.message}`)
-          }
-          throw error
-        })
-    })
-    .catch((error) => {
-      if (error.code === 'HTTP_ERROR') {
-        throw error
-      }
-      else if (error.response) {
-        console.error(`Error: Failed to load page ${url}. HTTP status: ${error.response.status}`)
-        const httpError = new Error(`HTTP ${error.response.status}`)
-        httpError.code = 'HTTP_ERROR'
-        throw httpError
-      }
-      else if (error.request) {
-        console.error(`Error: No response from ${url}. Check your network connection.`)
-        const networkError = new Error('Network error')
-        networkError.code = 'NETWORK_ERROR'
-        throw networkError
-      }
-      else if (error.code === 'EACCES' || error.code === 'ENOENT') {
-        throw error
-      }
-      else {
-        console.error(`Error: Failed to load page ${url}: ${error.message}`)
-        throw error
-      }
-    })
-};
-
-const isLocalResource = (pageUrl, resourceSrc) => {
-  try {
-    const pageHost = new URL(pageUrl).hostname
-    const resourceUrl = new URL(resourceSrc, pageUrl)
-    const resourceHost = resourceUrl.hostname
-
-    return resourceHost === pageHost || resourceHost.endsWith(`.${pageHost}`)
-  }
-  catch {
-    return false
-  }
-}
-
-const downloadResource = (resourceUrl, resourcePath) => {
-  return axios.get(resourceUrl, { responseType: 'arraybuffer' })
-    .then((response) => {
-      if (response.status !== 200) {
-        throw new Error(`HTTP ${response.status}`)
-      }
-      return fs.writeFile(resourcePath, response.data)
-    })
-    .then(() => ({
-      originalUrl: resourceUrl,
-      localPath: resourcePath,
-      status: 'success',
-    }))
-    .catch((error) => {
-      let errorMessage = error.message
-
-      if (error.response) {
-        errorMessage = `HTTP ${error.response.status}`
-      }
-      else if (error.request) {
-        errorMessage = 'Network error'
-      }
-      else if (error.code === 'EACCES') {
-        errorMessage = 'Permission denied'
-      }
-      else if (error.code === 'ENOENT') {
-        errorMessage = 'Directory does not exist'
-      }
-
-      console.error(`Warning: Failed to download resource ${resourceUrl}: ${errorMessage}`)
-
-      throw error
-    })
-}
 
 function pageloader(url, dir = process.cwd()) {
   log('=== Starting pageloader ===')
@@ -120,176 +24,44 @@ function pageloader(url, dir = process.cwd()) {
   let htmlData
   let $
   let dirPath
+  let localResources
+  let downloadResults
 
-  return fs.access(dir)
-    .catch((error) => {
-      if (error.code === 'ENOENT') {
-        return
-      }
-      throw error
-    })
-    .then(() => fs.mkdir(dir, { recursive: true }))
-    .catch((error) => {
-      if (error.code === 'EACCES') {
-        console.error(`Error: Permission denied. Cannot create directory ${dir}`)
-      }
-      else if (error.code === 'ENOTDIR') {
-        console.error(`Error: ${dir} exists but is not a directory`)
-      }
-      else {
-        console.error(`Error: Failed to create directory ${dir}: ${error.message}`)
-      }
-      throw error
-    })
+  return Promise.resolve()
+    .then(() => isValidUrl(url))
+    .then(() => checkDirectory(dir))
     .then(() => getHtmlPage(url, dir))
     .then((result) => {
       htmlData = result
       const dirName = getDirName(url)
       dirPath = path.join(dir, dirName)
-
-      return fs.mkdir(dirPath, { recursive: true })
-        .catch((error) => {
-          if (error.code === 'EACCES') {
-            console.error(`Error: Permission denied. Cannot create directory ${dirPath}`)
-          }
-          else if (error.code === 'ENOTDIR') {
-            console.error(`Error: ${dirPath} exists but is not a directory`)
-          }
-          else {
-            console.error(`Error: Failed to create resourses directory ${dirPath}: ${error.message}`)
-          }
-          throw error
-        })
     })
+    .then(() => checkDirectory(dirPath))
     .then(() => {
       $ = cheerio.load(htmlData.htmlContent)
-
-      const localResources = []
-      $('img').each((i, element) => {
-        const src = $(element).attr('src')
-        if (src && isLocalResource(url, src)) {
-          const fullResourceUrl = new URL(src, url).href
-          const resourceName = getAssetPath(url, src)
-          const resourcePath = path.join(dir, resourceName)
-
-          localResources.push({
-            element,
-            tagName: 'img',
-            attrName: 'src',
-            originalValue: src,
-            fullUrl: fullResourceUrl,
-            localPath: resourcePath,
-            relativePath: resourceName,
-          })
-        }
-      })
-
-      $('link').each((i, element) => {
-        const href = $(element).attr('href')
-        if (href && isLocalResource(url, href)) {
-          const fullResourceUrl = new URL(href, url).href
-          const resourceName = getAssetPath(url, href)
-          const resourcePath = path.join(dir, resourceName)
-
-          localResources.push({
-            element,
-            tagName: 'link',
-            attrName: 'href',
-            originalValue: href,
-            fullUrl: fullResourceUrl,
-            localPath: resourcePath,
-            relativePath: resourceName,
-          })
-        }
-      })
-
-      $('script').each((i, element) => {
-        const src = $(element).attr('src')
-        if (src && isLocalResource(url, src)) {
-          const fullResourceUrl = new URL(src, url).href
-          const resourceName = getAssetPath(url, src)
-          const resourcePath = path.join(dir, resourceName)
-
-          localResources.push({
-            element,
-            tagName: 'script',
-            attrName: 'src',
-            originalValue: src,
-            fullUrl: fullResourceUrl,
-            localPath: resourcePath,
-            relativePath: resourceName,
-          })
-        }
-      })
+      localResources = findLocalResources(url, $, dir)
+    })
+    .then(() => {
       if (localResources.length === 0) {
-        console.log('No local resources found to download')
-        return { localResources, downloadResults: [] }
+        log('No local resources found to download')
+        return
       }
-      const tasks = localResources.map(resource => ({
-        title: `${path.basename(resource.localPath)}`,
-        task: () => downloadResource(resource.fullUrl, resource.localPath),
-      }))
-
-      const listr = new Listr(tasks, {
-        concurrent: true,
-        exitOnError: false,
-      })
-
-      // log('Found %d local resources to download', localResources.length)
-
+      const tasks = createDownloadTasks(localResources)
+      const listr = new Listr(tasks, { concurrent: true, exitOnError: false })
       return listr.run()
-        .then(() => {
-          return Promise.allSettled(
-            localResources.map(resource =>
-              fs.access(resource.localPath).then(() => ({
-                originalUrl: resource.fullUrl,
-                localPath: resource.localPath,
-                status: 'success',
-              })).catch(() => ({
-                originalUrl: resource.fullUrl,
-                localPath: resource.localPath,
-                status: 'error',
-              })),
-            ),
-          )
-        })
-        .then((results) => {
-          const downloadResults = results.map(r => r.value)
-          return { localResources, downloadResults }
-        })
     })
-    .then(({ localResources, downloadResults }) => {
-      localResources.forEach((resource, index) => {
-        const downloadResult = downloadResults[index]
-        if (downloadResult.status === 'success') {
-          $(resource.element).attr(resource.attrName, resource.relativePath)
-        }
-      })
-
-      const updatedHtml = $.html()
-      return fs.writeFile(htmlData.filePath, updatedHtml)
-
-        .catch((error) => {
-          if (error.code === 'EACCES') {
-            console.error(`Error: Permission denied. Cannot update HTML file ${htmlData.filePath}`)
-          }
-          else if (error.code === 'ENOENT') {
-            console.error(`Error: Directory does not exist for file ${htmlData.filePath}`)
-          }
-          else {
-            console.error(`Error: Failed to update HTML file: ${error.message}`)
-          }
-          throw error
-        })
+    .then((results) => {
+      downloadResults = results || []
+      if (localResources.length > 0) {
+        updateHtmlWithLocalPaths($, localResources, downloadResults)
+      }
     })
+    .then(() => fs.writeFile(htmlData.filePath, $.html()))
     .then(() => ({
       htmlFilePath: htmlData.filePath,
       dirName: path.basename(dirPath),
       dirPath,
     }))
-    .catch((error) => {
-      throw error
-    })
 }
 
-export { pageloader }
+ export { pageloader }
